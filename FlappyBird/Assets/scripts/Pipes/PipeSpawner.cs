6ea914762a73d6296f899b0f040c386a3136de5f
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-05-27 18:15:50 +0530

    Pipe spawning

--- FlappyBird/Assets/scripts/Pipes/PipeSpawner.cs
+++ FlappyBird/Assets/scripts/Pipes/PipeSpawner.cs
@@ -5,15 +5,33 @@ using UnityEngine;
 public class PipeSpawner : MonoBehaviour
 {
     public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
+    public float hieghtOffset = 10;
 
     void Start()
     {
-        
+        spawnPipe();
     }
 
   
     void Update()
     {
-        
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            spawnPipe();
+            timer = 0;
+        }
+    }
+    void spawnPipe()
+    {
+        float lowestPoint = transform.position.y - hieghtOffset;
+        float highestPoint = transform.position.y + hieghtOffset;
+
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0),transform.rotation);
     }
 }
