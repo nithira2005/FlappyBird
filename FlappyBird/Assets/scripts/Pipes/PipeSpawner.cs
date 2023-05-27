4ca14fc1aa19bfb83a663e19d434f67a6de83536
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-05-27 17:58:21 +0530

    Spawner to spawn pipes

---
+++ FlappyBird/Assets/scripts/Pipes/PipeSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public GameObject pipe;
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
