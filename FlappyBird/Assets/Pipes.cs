07986782b25ade301b9b46c9149953bac0d91374
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-05-27 17:54:33 +0530

    Pipe speed

--- FlappyBird/Assets/Pipes.cs
+++ FlappyBird/Assets/Pipes.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Pipes : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5;
+   
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
     }
 }
