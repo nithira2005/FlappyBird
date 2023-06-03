f3b310d27c90179460bd95822c9911d031bba0f0
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-06-03 17:00:31 +0530

    Game is Done :)

--- FlappyBird/Assets/scripts/Bird/Bird.cs
+++ FlappyBird/Assets/scripts/Bird/Bird.cs
@@ -6,18 +6,27 @@ public class Bird : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrenghth;
+    public LogicScript logic;
+    public bool birdAlive = true;
+
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+
     }
 
-  
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space)&& birdAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrenghth;
         }
-       
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdAlive = false;
+    }
+
 }
